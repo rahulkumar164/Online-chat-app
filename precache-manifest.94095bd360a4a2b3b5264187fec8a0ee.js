fd26be54d193a2b3664a977e925b6fe60cb57cd9
rahulkumar164 <41946007+rahulkumar164@users.noreply.github.com>
2021-09-10 18:29:59 +0530

    Updates

---
+++ precache-manifest.94095bd360a4a2b3b5264187fec8a0ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f57da8e8c61ef3e1cd79ca75b873f17",
+    "url": "/Online-chat-app/index.html"
+  },
+  {
+    "revision": "79e126ad00b1cac26f39",
+    "url": "/Online-chat-app/static/css/main.33f080b6.chunk.css"
+  },
+  {
+    "revision": "404516f8ec29df3f610a",
+    "url": "/Online-chat-app/static/js/2.92bbcb42.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/Online-chat-app/static/js/2.92bbcb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e126ad00b1cac26f39",
+    "url": "/Online-chat-app/static/js/main.0fcd813d.chunk.js"
+  },
+  {
+    "revision": "0c039a715893952030f9",
+    "url": "/Online-chat-app/static/js/runtime-main.a969d0e5.js"
+  }
+]);
